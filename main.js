368e138b2dcf5f7a6affac39ebe2cb5462865763
Borys Kharchenko <arximus88@gmail.com>
2024-06-30 20:59:49 +0300

    clocks

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
-const { app, BrowserWindow, Tray, Menu } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Tray, Menu } = require("electron");
+const path = require("path");
 
 let tray = null;
 let widgetWindows = {};
 
 const widgetDefinitions = {
   clock: {
-    name: 'Clock',
+    name: "Clock",
     width: 200,
     height: 100,
-    file: 'clock.html'
+    file: "clock.html",
+  },
+  "clock-simple": {
+    name: "Clock Simple",
+    width: 200,
+    height: 100,
+    file: "clock-simple.html",
   },
   // Add more widget definitions here
 };
 
 function createTray() {
-  tray = new Tray(path.join(__dirname, 'icon.png'));
+  tray = new Tray(path.join(__dirname, "icon.png"));
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Widgets', submenu: buildWidgetSubmenu() },
-    { type: 'separator' },
-    { label: 'Quit', click: () => app.quit() }
+    { label: "Widgets", submenu: buildWidgetSubmenu() },
+    { type: "separator" },
+    { label: "Quit", click: () => app.quit() },
   ]);
-  tray.setToolTip('Widget Manager');
+  tray.setToolTip("Widget Manager");
   tray.setContextMenu(contextMenu);
 }
 
 function buildWidgetSubmenu() {
-  return Object.keys(widgetDefinitions).map(widgetKey => ({
+  return Object.keys(widgetDefinitions).map((widgetKey) => ({
     label: widgetDefinitions[widgetKey].name,
     submenu: [
-      { 
-        label: 'Run', 
+      {
+        label: "Run",
         click: () => createWidgetWindow(widgetKey),
-        enabled: !widgetWindows[widgetKey]
+        enabled: !widgetWindows[widgetKey],
       },
-      { 
-        label: 'Close', 
+      {
+        label: "Close",
         click: () => closeWidget(widgetKey),
-        enabled: !!widgetWindows[widgetKey]
-      }
-    ]
+        enabled: !!widgetWindows[widgetKey],
+      },
+    ],
   }));
 }
 
@@ -54,18 +60,18 @@ function createWidgetWindow(widgetKey) {
     transparent: true,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
     },
-    skipTaskbar: true
+    skipTaskbar: true,
   });
 
   win.loadFile(widget.file);
-  win.setAlwaysOnTop(true, 'screen-saver');
+  win.setAlwaysOnTop(true, "screen-saver");
   win.setVisibleOnAllWorkspaces(true);
 
   widgetWindows[widgetKey] = win;
 
-  win.on('closed', () => {
+  win.on("closed", () => {
     delete widgetWindows[widgetKey];
     updateTrayMenu();
   });
@@ -81,9 +87,9 @@ function closeWidget(widgetKey) {
 
 function updateTrayMenu() {
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Widgets', submenu: buildWidgetSubmenu() },
-    { type: 'separator' },
-    { label: 'Quit', click: () => app.quit() }
+    { label: "Widgets", submenu: buildWidgetSubmenu() },
+    { type: "separator" },
+    { label: "Quit", click: () => app.quit() },
   ]);
   tray.setContextMenu(contextMenu);
 }
@@ -92,8 +98,8 @@ app.whenReady().then(() => {
   createTray();
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
